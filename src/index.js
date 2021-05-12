e3074184933582a715518f866a474a3d5b858cd1
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-05-12 07:29:34 -0300

    metodos http

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get("/",(request, response)=> {
-    return response.json({message: "Hello World Ignite"})
+app.get("/courses",(request, response)=> {
+    return response.json({message: ["Curso 1", "Curso 2", "Curso 3"]})
 });
 
+app.post("/courses", (request, response)=> {
+    return response.json({message: ["Curso 1", "Curso 2", "Curso 3", "Curso 4"]})
+
+});
+
+app.put("/courses/:id", (request, response)=> {
+    return response.json({message: ["Curso 6", "Curso 2", "Curso 3"]})
+});
+
+app.patch("/courses/:id", (resquest, response)=> {
+    return response.json({message: ["Curso 1", "Curso 7", "Curso 3"]})
+});
+
+app.delete("/courses/:id", (resquest, response)=>{
+    return response.json({message: ["Curso 1", "Curso 2"]})
+})
 app.listen(3333);
